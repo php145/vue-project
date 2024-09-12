62c870d7506010a94cde8eeb697cb657d43afb07
145 <7930902+php145@user.noreply.gitee.com>
2024-09-12 14:16:00 +0800

    引入elementui

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,53 @@
-<script setup>
-</script>
-
 <template>
-
-  <div>
-    11111
+  <div class="common-layout">
+    <el-container>
+      <el-header>养老金计算器</el-header>
+      <el-main>Main</el-main>
+    </el-container>
   </div>
 </template>
+<script setup>
+
+</script>
+
+<style>
+  .el-container{
+    margin: 0;
+    padding: 0;
+    height: 100vh;
+  }
+
+  .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+  
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+  
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+  
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+  
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
+</style>
